ef73aa897a46862b27b681db9c3e53ab152506f3
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-06 13:52:50 +0100

    exo partie1 MySql

---
+++ partie1/script.sql
@@ -0,0 +1,12 @@
+--Partie 1
+--Exercice 1
+--Création d'une base de donnée (BDD) nommée languages
+CREATE DATABASE `languages` ;
+--Création d'une base de donnée (BDD) nommée languages si elle n'existe pas (bonne pratique !)
+CREATE DATABASE IF NOT EXISTS `languages` ;
+--Exercice 2
+--Création d'une base de donnée nommée webDevelopment avec l'encodage utf8
+CREATE DATABASE IF NOT EXISTS `webDevelopment` CHARACTER SET 'utf8' ;
+--Exercice 3
+--Création d'une base de donnée nommée framework avec l'encodage utf8
+CREATE DATABASE IF NOT EXISTS `framework` CHARACTER SET 'utf8'
