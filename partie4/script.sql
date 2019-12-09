d712e5caffd471718c70ffcff4dc7a845a2cfc5e
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-09 14:32:53 +0100

    correction faites

--- partie4/script.sql
+++ partie4/script.sql
@@ -1,11 +1,25 @@
 --Partie 4 Insertion de donnée
 --Exercice 1
+-- j'utilise ma base de donnée webDevelopment
 USE `webdevelopment` ;
 --Dans le tableau langages, insérer dans les  2 colonnes (languages, version) les différentes valeurs VALUES
 INSERT INTO `languages`(`languages`,`version`)
-VALUES ('JavaScript','5'), ('PHP', '5.2'), ('PHP', '5.4'), ('HTML', '5.1'), ('JavaScript', '6'), ('JavaScript', '7'), ('JavaScript', '8'), ('PHP', '7');
+VALUES
+-- colonne type VARCHAR donc caractère alors on entour de côtes simple les chiffres et les noms de versions
+('JavaScript','5'),
+('PHP', '5.2'),
+('PHP', '5.4'),
+('HTML', '5.1'),
+('JavaScript', '6'),
+('JavaScript', '7'),
+('JavaScript', '8'),
+('PHP', '7');
 --Exercice 2
 USE `webdevelopment` ;
 --Dans le tableau framework, insérer dans les  2 colonnes (name, version) les différentes valeurs VALUES
 INSERT INTO `framework`(name, version)
-VALUES ('Symfony','2.8'),('Symfony', '3'),('Jquery', '1.6'),('Jquery', '2.10');
+VALUES
+('Symfony','2.8'),
+('Symfony', '3'),
+('Jquery', '1.6'),
+('Jquery', '2.10');
