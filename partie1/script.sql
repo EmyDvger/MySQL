f7b88957565d51e8178fb802a03d650aeb73bd8c
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-09 09:32:30 +0100

    correction partie1 SQL

--- partie1/script.sql
+++ partie1/script.sql
@@ -5,14 +5,17 @@ CREATE DATABASE `languages` ;
 --Création d'une base de donnée (BDD) nommée languages si elle n'existe pas (bonne pratique !)
 CREATE DATABASE IF NOT EXISTS `languages` ;
 --Exercice 2
---Création d'une base de donnée nommée webDevelopment avec l'encodage utf8
-CREATE DATABASE IF NOT EXISTS `webDevelopment` CHARACTER SET 'utf8' ;
+--Création d'une base de donnée nommée webDevelopment avec l'encodage utf8 avec les deux solutions
+CREATE DATABASE  `webDevelopment` CHARACTER SET 'utf8' ;
+CREATE DATABASE  `webDevelopment` CHARSET='utf8' ;
 --Exercice 3
---Création d'une base de donnée nommée framework avec l'encodage utf8
-CREATE DATABASE IF NOT EXISTS `framework` CHARACTER SET 'utf8'
+--Création d'une base de donnée nommée framework avec l'encodage utf8 si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET 'utf8' ;
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARSET='utf8' ;
 --Exercice 4
 --Supprimer une base de donnée
 DROP DATABASE `languages` ;
 --Exercice 5
 --Supprimer une base de donnée si elle existe
+--Commande dangereuse en ligne de commande, très faire attention car impossible de restaurer la BDD
 DROP DATABASE IF EXISTS `framework` ;
